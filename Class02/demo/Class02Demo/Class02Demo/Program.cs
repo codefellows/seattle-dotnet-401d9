729f927ebdf81200118d43c59fafc7d17453f98f
Amanda Iverson <ajohnson764@gmail.com>
2019-10-15 11:31:40 -0700

    added xunit testing project

--- Class02/demo/Class02Demo/Class02Demo/Program.cs
+++ Class02/demo/Class02Demo/Class02Demo/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Class02Demo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MyName("amanda");
+        }
+
+        /// <summary>
+        /// Outputs to the console the name of the user and generates the product for all numbers in an array
+        /// </summary>
+        /// <param name="name">the name of the user</param>
+        public static void MyName(string name)
+        {
+            Console.WriteLine($"My name is {name}");
+
+            string candy = "gummy bears";
+
+            int[] myArray = { 1, 2, 3, 4, 5, 6, 7 };
+            int product = 1;
+
+            // gets the product of all values in the array
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                product *= myArray[i];
+            }
         }
     }
 }
---
+++ Class02/demo/Class02Demo/FizzBuzzTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static Class02Demo.Program;
+
+namespace FizzBuzzTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
