90906138baaf5c556a73ea392e2a9c971f15de80
Amanda Iverson <ajohnson764@gmail.com>
2019-11-19 17:11:16 -0800

    adding 2 sets of demo code

---
+++ Class27/demo/CMSCohort9/CMSCohort9/Data/BlogDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CMSCohort9.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSCohort9.Data
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext(DbContextOptions<BlogDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ Class27/demo/CMSCohort9/CMSCohort9/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMSCohort9.Data;
+using CMSCohort9.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CMSCohort9
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // register the database for non user related items
+            services.AddDbContext<BlogDBContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("BlogDefaultConnection")));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("UserDefaultConnection")));
+
+            // Connect the User with the specific Database to store the information
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable the use of Authentication
+            app.UseAuthentication();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
