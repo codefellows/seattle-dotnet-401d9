c86bb075e0a227ba0e32845befbdcaa2caed077f
Amanda Iverson <ajohnson764@gmail.com>
2019-11-25 16:53:16 -0800

    add demo and code challenge

---
+++ Class31/CodeChallenges/Challenge.md
@@ -0,0 +1,3 @@
+# Code Challenge Practice
+
+1. Create a function that takes in a tree and returns whether or not the tree is a Binary Search Tree.
---
+++ Class31/Demo/CMSCohort9/CMSCohort9/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CMSCohort9.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSCohort9.Data
+{
+    // Inherit from the identity db context with some basic tables
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
