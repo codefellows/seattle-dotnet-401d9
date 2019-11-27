6ea768e49bd71abadb9c70b8f4e4a98fffa9765f
Amanda Iverson <ajohnson764@gmail.com>
2019-11-26 16:51:30 -0800

    add class demo

---
+++ Class32/CMSCohort9/CMSCohort9/Models/Services/PostService.cs
@@ -0,0 +1,42 @@
+﻿using CMSCohort9.Data;
+using CMSCohort9.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSCohort9.Models.Services
+{
+    public class PostService : IPostManagement
+    {
+        private BlogDBContext _context;
+
+        public PostService(BlogDBContext context)
+        {
+            _context = context;
+        }
+        public async Task CreatePostAsync(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Post>> GetAllPostsAsync() => await _context.Posts.ToListAsync();
+
+        public async Task<Post> GetPostByIdAsync(int id) => await _context.Posts.FindAsync(id);
+
+        public async Task RemovePostAsync(int id)
+        {
+            var post = await GetPostByIdAsync(id);
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdatePostAsync(Post post)
+        {
+            _context.Posts.Update(post);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
